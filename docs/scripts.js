335b7ff7404f7e1042f09d055c3f43ce38901092
Ryan Christie <rcchris@ilstu.edu>
2019-01-03 17:13:17 -0600

    Add instructions and additional support files and boilerplate code

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,2 +1,13 @@
 // Add your Vue.js code
 console.log('start script.js');
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      people: []
+    },
+    mounted: function() {
+        
+    }
+  })
